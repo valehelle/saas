9cdf8b69ca19e9775af4e4bb37ef07171ebe7a71
Valehelle <hazmiirfan92@gmail.com>
2017-12-18 23:10:49 +0800

    change home style

--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,5 +1,30 @@
 class FrontController < ApplicationController
+    
     def index
+
+    end
+    
+    def about
+    
+    end
+    
+    def contact
+    
+    end
+    
+    def gallery
+    
+    end
+    
+    def faq
+    
+    end
+
+    def service
+    
+    end
+    
+    def material
 
     end
 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :students
   devise_for :teachers
   root to: 'front#index'
-  get '/document/', to: 'front#new', as: 'documents'
-  post '/document/', to: 'front#create'
+  get '/about/', to: 'front#about'
+  get '/gallery/', to: 'front#gallery'
+  get '/contact/', to: 'front#contact'
+  get '/faq/', to: 'front#faq'
+  get '/service/', to: 'front#service'
+  get '/material/', to: 'front#material'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
