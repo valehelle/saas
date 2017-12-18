22d74c8c26a5bb043e8b9010e4ca937792ddbead
hazmi irfan <hazmi@lelong.com.my>
2017-12-18 17:54:52 +0800

    add side bar

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,5 +11,32 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require turbolinks
 //= require_tree .
+$(document).ready(function () {
+    var trigger = $('.hamburger'),
+        overlay = $('.overlay'),
+       isClosed = false;
+  
+      trigger.click(function () {
+        hamburger_cross();      
+      });
+  
+      function hamburger_cross() {
+  
+        if (isClosed == true) {          
+          overlay.hide();
+          trigger.removeClass('is-open');
+          trigger.addClass('is-closed');
+          isClosed = false;
+        } else {   
+          overlay.show();
+          trigger.removeClass('is-closed');
+          trigger.addClass('is-open');
+          isClosed = true;
+        }
+    }
+    
+    $('[data-toggle="offcanvas"]').click(function () {
+          $('#wrapper').toggleClass('toggled');
+    });  
+  });
--- app/controllers/front_controller.rb
+++ app/controllers/front_controller.rb
@@ -1,13 +1,6 @@
 class FrontController < ApplicationController
     def index
-        @documents = Subject.last.documents.new
+
     end
-    def new
-    end
-    def create
-        @document = Subject.last.documents.new()
-        @document.doc = params['document']['doc']
-        puts 'helooo'
-        @document.save
-    end
+
 end
