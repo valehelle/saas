279a0767e250177226d993a7ccdabe2d85993a83
hazmi irfan <hazmi@lelong.com.my>
2017-12-19 16:15:25 +0800

    add active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   devise_for :students
   devise_for :teachers
   root to: 'front#index'
