48d188422a0d68233b5309c9e9c4f58e130cb2af
hazmi irfan <hazmi@lelong.com.my>
2017-12-19 15:46:17 +0800

    change styling

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+  devise :database_authenticatable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :enrolls
   has_many :subject, through: :enrolls
