5dd3cff78d0bacf6079a7da66d9de40504a9a8fb
Valehelle <hazmiirfan92@gmail.com>
2018-01-14 19:12:42 +0800

    fix in backend for seperation

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,7 +2,7 @@ class DashboardController < ApplicationController
     before_action :authenticate_user!
     layout "backend_application"
     def index
-        @announcements = Announcement.last(5).reverse
+        @announcements = Announcement.where(company_id: current_user.info.company_id).last(5).reverse
         if current_user.info.is_teacher then
             @subjects = current_user.teaches
         else
