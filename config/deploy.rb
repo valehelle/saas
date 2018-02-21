95c91eb0882ad2d1d566b8b7343555002e76ceab
hazmi irfan <hazmi@lelong.com.my>
2018-02-21 12:15:20 +0800

    remove

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,6 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.10.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
